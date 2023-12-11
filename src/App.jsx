f5257eb21804150605f3ab1400ef5e0f50138078
jcsii98 <jose.c.saribong@gmail.com>
2023-12-11 16:38:20 +0800

    basic todo functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // check localstorage for todos
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos")) || []
+  );
+
+  const displayTodos = todos.map((todo) => (
+    <li key={todo.id} className="text-xl">
+      {todo.task} {todo.completed ? "Completed" : "Pending"}
+    </li>
+  ));
+
+  // set todos
+  const [todo, setTodo] = useState("");
+
+  const inputChange = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log("handleSubmit called!");
+    console.log(todo);
+
+    // add todo to todos array
+    setTodos((current) => [
+      ...current,
+      {
+        id: todos.length,
+        task: todo,
+        completed: false,
+      },
+    ]);
+
+    // reset input field
+    setTodo("");
+  };
+
+  // useEffect to set to localStorage todos whenever todos is updated
+  useEffect(() => {
+    console.log(todos);
+    const todosString = JSON.stringify(todos);
+    localStorage.setItem("todos", todosString);
+  }, [todos]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-screen h-screen p-6">
+        <div
+          id="header"
+          className="w-full flex flex-col space-y-4 justify-center items-center bg-slate-600 rounded-md p-4"
+        >
+          <h1 className="text-3xl font-bold text-slate-100">
+            Jose's Todo List
+          </h1>
+          <div id="main-container" className="w-full flex space-x-4">
+            <div className="w-full bg-slate-100 rounded-md p-4">
+              <p className="text-xl font-medium text-slate-600">Tasks</p>
+              <div className="">
+                <ul>{displayTodos}</ul>
+              </div>
+              {/* map todos here */}
+            </div>
+            <div className="w-full bg-slate-100 rounded-md p-4">
+              <p className="text-xl font-medium text-slate-600 pb-2">
+                Add Task
+              </p>
+              <div className="flex flex-col space-y-4">
+                <input
+                  className="rounded-md w-full focus:outline-none p-2"
+                  type="text"
+                  name="todo"
+                  value={todo}
+                  onChange={inputChange}
+                  placeholder="Todo Description Here"
+                ></input>
+                <button
+                  className="p-4 bg-slate-600 rounded-md text-slate-100 text-xl font-medium"
+                  type="button"
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
